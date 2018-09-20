a9fd78ce505c2930afc6e2770621e2dd216bca7f
lagron1000 <lagron1000@gmail.com>
2018-09-21 00:10:43 +0300

    working but incorrectly

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-var socket = io();
+// var socket = io();
 // var audio = new Audio('assets\laugh\laugh.mp3');
 // var laughBtn = document.getElementById("laugh1"); 
 // const laf = () => {
@@ -6,40 +6,43 @@ var socket = io();
 // }
 // console.log(audio)
 // laughBtn.onclick = laf
-$('#laugh1').click(()=>{
-    socket.emit('ding', 'la')
-    return false;
-})
+// $('#laugh1').click(()=>{
+//     socket.emit('ding', 'la')
+//     return false;
+// })
 
-socket.on('ding', ()=>{
-    console.log('test2')
-})
+// socket.on('ding', ()=>{
+//     console.log('test2')
+// })
 
-// var LULCounter = 0;
-// var options = {
-//     options: {
-//         debug: true
-//     },
-//     connection: {
-//         cluster: "aws",
-//         reconnect: true
-//     },
-//     identity: {
-//         username: "LaughTrackBot",
-//         password: "oauth:5vhuaq4arzqv3woh3p4wfbgbjefnrf"
-//     },
-//     channels: ["lagron1000"]
-// };
+var LULCounter = 0;
+// let checkLUL = setInterval(()=>{LULCounter = 0}, 5000)
+var options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        cluster: "aws",
+        reconnect: true
+    },
+    identity: {
+        username: "LaughTrackBot",
+        password: "oauth:5vhuaq4arzqv3woh3p4wfbgbjefnrf"
+    },
+    channels: ["lagron1000"]
+};
 
-// var client = new tmi.client(options);
-// client.on("chat", function (channel, userstate, message, self) {
-//     if (self) return;
-//     if (message == "LUL") {
-//         LULCounter++;
-//         if (LULCounter >= 3) {
-//             client.say(channel, "hahahaha");
-//             LULCounter = 0
-//         }
-//     }
-// });
-// client.connect();
+var client = new tmi.client(options);
+client.on("chat", function (channel, userstate, message, self) {
+    if (self) return;
+    if (message == "LUL") {
+        LULCounter++;
+        if (LULCounter >= 6) {
+            client.say(channel, "hahahaha");
+            LULCounter = 0
+            document.getElementById('player').play()
+            // clearInterval(checkLUL)
+        }
+    }
+});
+client.connect();
--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-var tmi = require('tmi.js')
+// var tmi = require('tmi.js')
 var express = require('express');
 var bodyParser = require('body-parser');
 // var app = express();
 // var http = require('http').Server(app);
 // var io = require('socket.io')(http);
 var app = express();
-var server = app.listen(8080);
-var io = require('socket.io').listen(server);
+// var server = app.listen(8080);
+// var io = require('socket.io').listen(server);
 
-var LULCounter = 0;
-var options = {
-    options: {
-        debug: true
-    },
-    connection: {
-        cluster: "aws",
-        reconnect: true
-    },
-    identity: {
-        username: "LaughTrackBot",
-        password: "oauth:5vhuaq4arzqv3woh3p4wfbgbjefnrf"
-    },
-    channels: ["lagron1000"]
-};
+// var LULCounter = 0;
+// var options = {
+//     options: {
+//         debug: true
+//     },
+//     connection: {
+//         cluster: "aws",
+//         reconnect: true
+//     },
+//     identity: {
+//         username: "LaughTrackBot",
+//         password: "oauth:5vhuaq4arzqv3woh3p4wfbgbjefnrf"
+//     },
+//     channels: ["lagron1000"]
+// };
 
-var client = new tmi.client(options);
+// var client = new tmi.client(options);
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.use(bodyParser.json());
@@ -34,34 +34,34 @@ app.use(bodyParser.json());
 //   });
 
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('ding', function (msg) {
-        console.log(msg)
-        io.emit('ding', msg);
-        // socket.broadcast.emit('hi');
-    });
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-});
+// io.on('connection', function (socket) {
+//     console.log('a user connected');
+//     socket.on('ding', function (msg) {
+//         console.log(msg)
+//         io.emit('ding', msg);
+//         // socket.broadcast.emit('hi');
+//     });
+//     socket.on('disconnect', function () {
+//         console.log('user disconnected');
+//     });
+// });
 
 
 
-client.on("chat", function (channel, userstate, message, self) {
-    if (self) return;
-    if (message == "LUL") {
-        LULCounter++;
-        if (LULCounter >= 3) {
-            client.say(channel, "hahahaha");
-            LULCounter = 0
-        }
-    }
-});
+// client.on("chat", function (channel, userstate, message, self) {
+//     if (self) return;
+//     if (message == "LUL") {
+//         LULCounter++;
+//         if (LULCounter >= 3) {
+//             client.say(channel, "hahahaha");
+//             LULCounter = 0
+//         }
+//     }
+// });
 
 
-client.connect();
-// app.listen(process.env.PORT || '8080', () => {
-//     console.log("Server started on port 8080");
-//   });
+// client.connect();
+app.listen(process.env.PORT || '8080', () => {
+    console.log("Server started on port 8080");
+  });
 
