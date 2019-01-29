63fcc8e2558e2c2da1552964cb71937872acd850
lagron1000 <lagron1000@gmail.com>
2019-01-29 22:48:57 +0200

    fixed server side and now works with just a slight delay

--- public/main.js
+++ public/main.js
@@ -1,7 +1,16 @@
 // let checkLUL = setInterval(function(){LULCounter = 0}, 5000)
-import rules from '/rules.js';
-// var reg = /\b(lul|lol|ha)+\b/gi
-var LULCounter = 0;
+
+Pusher.logToConsole = true;
+
+var pusher = new Pusher('bd3c6c36ae064635c676', {
+    cluster: 'ap2',
+    forceTLS: true
+});
+var channel = pusher.subscribe('my-channel');
+channel.bind('my-event', function (data) {
+    console.log(data.dir)
+    $(data.dir).get(0).play()
+});
 Twitch.init({ clientId: '0swf50e2k1wc7r4d10qqirz6g74n37' }, function (error, status) {
     if (error) { console.log(error) }
     if (status) {
@@ -9,37 +18,13 @@ Twitch.init({ clientId: '0swf50e2k1wc7r4d10qqirz6g74n37' }, function (error, sta
             $('#login').hide()
             $('#logout').show()
             Twitch.api({ method: 'channel' }, function (error, channel) {
-                var options = {
-                    options: {
-                        debug: true
-                    },
-                    connection: {
-                        cluster: "aws",
-                        reconnect: true
-                    },
-                    identity: {
-                        username: "LaughTrackBot",
-                        password: "oauth:5vhuaq4arzqv3woh3p4wfbgbjefnrf"
-                    },
-                    channels: [channel.display_name]
-                };
-                var client = new tmi.client(options);
-                client.on("chat", function (channel, userstate, message, self) {
-                    if (self) return;
-                    for (let i = 0; i < rules.length; i++) {
-                        if (rules[i].keyWords.test(message)) {
-                            console.log(rules[i].keyWords.test(message))
-                            rules[i].counter++;
-                        }
-                        if (rules[i].counter >= 2) {
-                            rules[i].responseDir.play()
-                            client.say(channel, rules[i].responseText);
-                            rules[i].counter = 0
-                            // clearInterval(checkLUL)
-                        }
-                    }
+                $.ajax({
+                    type : "POST",
+                    url : "http://localhost:8080/endpoint",
+                    data : channel,
+                    dataType : 'application/json'
                 });
-                client.connect();
+                return;
             });
             $('#guide').hide();
             $('#main').show();
@@ -56,8 +41,15 @@ $('#login').click(function () {
 })
 
 $('#logout').click(function () {
-    Twitch.logout(function (error) {
+    Twitch.logout(function (error, channel) {
+        $.ajax({
+            type : "POST",
+            url : "http://localhost:8080/disconnect",
+            data : channel,
+            dataType : 'application/json'
+        });
         location.reload();
+        return;
     });
 })
 
--- server.js
+++ server.js
@@ -1,47 +1,76 @@
-// var tmi = require('tmi.js')
+var tmi = require('tmi.js')
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-
-
-// var LULCounter = 0;
-// var options = {
-//     options: {
-//         debug: true
-//     },
-//     connection: {
-//         cluster: "aws",
-//         reconnect: true
-//     },
-//     identity: {
-//         username: "LaughTrackBot",
-//         password: "oauth:5vhuaq4arzqv3woh3p4wfbgbjefnrf"
-//     },
-//     channels: ["lagron1000"]
-// };
-
-// var client = new tmi.client(options);
+var Pusher = require('pusher');
+var rules = require('./rules').rules;
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false}));
 
-// client.on("chat", function (channel, userstate, message, self) {
-//     if (self) return;
-//     if (message == "LUL") {
-//         LULCounter++;
-//         if (LULCounter >= 3) {
-//             client.say(channel, "hahahaha");
-//             LULCounter = 0
-//         }
-//     }
-// });
-// client.connect();
+channelsArr = [];
 
-app.post('/sisma', function (req, res) {
-  res.send('oauth:5vhuaq4arzqv3woh3p4wfbgbjefnrf')
+var pusher = new Pusher({
+  appId: '605640',
+  key: 'bd3c6c36ae064635c676',
+  secret: '35432209834fea2cb351',
+  cluster: 'ap2',
+  encrypted: true
 });
 
+app.post('/endpoint', function(req, res){
+  channelsArr.push(req.body.display_name);
+  var options = {
+    options: {
+      debug: true
+    },
+    connection: {
+      cluster: "aws",
+      reconnect: true
+    },
+    identity: {
+      username: "LaughTrackBot",
+      password: "oauth:5vhuaq4arzqv3woh3p4wfbgbjefnrf"
+    },
+    channels: [req.body.display_name]
+  };
+
+var client = new tmi.client(options);
+
+
+client.on("chat", function (channel, userstate, message, self) {
+    if (self) return;
+    for (let i = 0; i < rules.length; i++){
+      if(rules[i].keyWords.test(message)){
+        rules[i].counter++;
+      }
+      if(rules[i].counter >= 2){
+        pusher.trigger('my-channel', 'my-event', {
+          "dir": rules[i].responseDir
+        });
+        client.say(channel, rules[i].responseText);
+        rules[i].counter = 0;
+      }
+    }
+});
+client.connect();
+})
+
+
+// app.post('/sisma', function (req, res) {
+//   res.send('oauth:5vhuaq4arzqv3woh3p4wfbgbjefnrf')
+// });
+app.post('/disconnect', function(req, res){
+  channelsArr.push(req.body.display_name);
+  for (let i = 0; i < channelsArr.length; i++){
+    if(channelsArr == req.body.display_name){
+      channelsArr.splice(i, 1);
+    }
+  }
+})
+
 app.listen(process.env.PORT || '8080', () => {
-    console.log("Server started on port 8080");
-  });
+  console.log("Server started on port 8080");
+});
 
